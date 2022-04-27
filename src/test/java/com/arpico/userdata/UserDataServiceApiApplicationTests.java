871fe65413aacb4f998838ec6cc328b86f295240
JaniAye <janiduayeshan16@gmail.com>
2022-04-27 22:30:48 +0530

    created besic api

---
+++ src/test/java/com/arpico/userdata/UserDataServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpico.userdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
